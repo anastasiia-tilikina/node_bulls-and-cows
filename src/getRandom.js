28726c0f328c3e488685a5eabd57de31cb33d7f5
Anastasiia Tilikina <anastasiia.tilikina@bndigital.co>
2023-08-15 21:27:41 +0300

    fix randomizer

--- src/getRandom.js
+++ src/getRandom.js
@@ -3,7 +3,24 @@
 const random = require(`lodash/random`);
 
 const getRandom = () => {
-  return random(1000, 9999);
+  const numbers = [];
+  let index = 0;
+
+  const randomizer = () => {
+    for (; index < 4; index++) {
+      const digit = index === 0 ? random(1, 9) : random(0, 9);
+
+      if (numbers.includes(digit)) {
+        randomizer();
+      } else {
+        numbers[index] = digit;
+      }
+    }
+
+    return +numbers.join('');
+  };
+
+  return randomizer();
 };
 
 module.exports = { getRandom };
